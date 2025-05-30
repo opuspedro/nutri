7c4005ee380fd616f93b86ae4530643361b52de8
[dyad] <t3xpromotion12@gmail.com>
2025-05-30 14:03:41 -0300

    [dyad] wrote 3 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -67,7 +67,7 @@ const Index = () => {
 
 
   return (
-    <div className="min-h-screen flex flex-col items-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-4">
+    <div className="min-h-screen flex flex-col items-center text-gray-900 dark:text-gray-100 p-4"> {/* Removed bg-gray-100 dark:bg-gray-900 */}
       <div className="text-center mb-8">
         <h1 className="text-4xl font-bold">Arquivos para Revisão</h1>
         <p className="text-xl text-gray-600 dark:text-gray-400 mt-2">

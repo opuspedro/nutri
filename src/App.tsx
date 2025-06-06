b15fb3774137cc3dc4e26c40e2c291cef0cbd1a3
[dyad] <t3xpromotion12@gmail.com>
2025-06-06 00:52:35 -0300

    [dyad] Added status column to files table and updated logic to filter pending files by status - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-// import ReviewFilesPage from "./pages/ReviewFilesPage"; // Remove old project review page
-import ReviewFilePage from "./pages/ReviewFilePage"; // Import the new file review page
+import ReviewFilePage from "./pages/ReviewFilePage";
 import HistoryPage from "./pages/HistoryPage";
 
 const queryClient = new QueryClient();
@@ -20,9 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          {/* Removed old project review route */}
-          {/* <Route path="/review/:projectId" element={<ReviewFilesPage />} /> */}
-          <Route path="/review-file/:fileId" element={<ReviewFilePage />} /> {/* Add the new file review route */}
+          <Route path="/review-file/:fileId" element={<ReviewFilePage />} />
           <Route path="/history" element={<HistoryPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
